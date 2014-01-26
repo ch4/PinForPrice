72b1b5533a2f9b96d0cd11ceebf001afa8de1e50
Harvey <ch4ch4@gmail.com>
2014-01-26 07:59:19 -0800

    added about and contact pages and dbproxy

---
+++ Azure/DBProxy.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="DBProxy.aspx.cs" Inherits="DBProxy" %>
+
+<asp:Literal runat="server" id="jsonResponse"></asp:Literal>
---
+++ Azure/DBProxy.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class DBProxy : System.Web.UI.Page
+{
+    protected void Page_Init(object sender, EventArgs e)
+    {
+        string id = Request.QueryString["id"];
+        string vote = Request.QueryString["vote"];
+        pinforpricedbEntities1 dbentities = new pinforpricedbEntities1();
+        
+        var item = dbentities.pinforpricedbs.FirstOrDefault(pinforpricedb => pinforpricedb.Pinterest_ID == id);
+
+        if (vote != "high" && vote != "low") {
+            //return the price
+            jsonResponse.Text = item == null ? "5" : item.Price.ToString();
+            return;
+        }
+
+        if (item == null) {
+            pinforpricedb newEntry = new pinforpricedb();
+            newEntry.Pinterest_ID = id;
+            newEntry.Price = 5; //start off at 5 bucks
+            newEntry.Votes_TooHigh = 0;
+            newEntry.Votes_TooLow = 0;
+            if (vote == "high") {
+                newEntry.Votes_TooHigh += 1;
+            } else if (vote == "low") {
+                newEntry.Votes_TooLow += 1;
+            }
+            newEntry.Price = CalcPrice(newEntry.Price, newEntry.Votes_TooHigh, newEntry.Votes_TooLow);
+            dbentities.pinforpricedbs.Add(newEntry);
+            dbentities.SaveChanges();
+        } else {
+            if (vote == "high") {
+                item.Votes_TooHigh += 1;
+            } else if (vote == "low") {
+                item.Votes_TooLow += 1;
+            }
+            item.Price = CalcPrice(item.Price, item.Votes_TooHigh, item.Votes_TooLow);
+            dbentities.Entry(item).State = System.Data.EntityState.Modified;
+            dbentities.SaveChanges();
+        }
+    }
+
+    public double CalcPrice(double currentPrice, int highVotes, int lowVotes) {
+        if (highVotes == 0) {
+            //this means the the votes are all too low
+            return currentPrice * 2;
+        } else if (lowVotes == 0) {
+            //this means the votes are all too high
+            return currentPrice / 2;
+        } else {
+            return currentPrice * (((double)lowVotes) / ((double)highVotes));
+        }
+
+    }
+}
